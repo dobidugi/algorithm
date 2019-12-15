f311427210b292bd1b9daada03879ee194cfb906
dobidugi <dobidugi@gmail.com>
2019-12-16 04:29:08 +0900

    잠안와서 2문제

---
+++ programmers/LEVEL2/JadenCase 문자열 만들기.js
@@ -0,0 +1,16 @@
+﻿function solution(s) {
+    var answer = '';
+    let re = "";
+    let list = s.split(" ");
+    for(let i=0;i<list.length;i++){
+        for(let j=0;j<list[i].length;j++){
+            if(j==0) answer+=list[i][j].toUpperCase();
+            else answer+=list[i][j].toLowerCase();
+        }
+        answer+=" ";
+    }
+    for(let i=0;i<answer.length-1;i++){
+        re+=answer[i];
+    }
+    return re;
+}
---
+++ programmers/LEVEL2/최대값과 최솟값.js
@@ -0,0 +1,9 @@
+﻿function solution(s) {
+    var answer = '';
+    let list = s.split(" ");
+    list.sort((a,b)=>a-b);
+    answer+= list[0];
+    answer+= " ";
+    answer+= list[list.length-1];
+    return answer;
+}
