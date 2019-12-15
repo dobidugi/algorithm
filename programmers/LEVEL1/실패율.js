db5b3704181383b0dd717418b145af1c302996e3
dobidugi <dobidugi@gmail.com>
2019-12-15 22:22:43 +0900

    level1 all clear

---
+++ programmers/LEVEL1/실패율.js
@@ -0,0 +1,40 @@
+﻿// 2019 카카오톡 블라인드 테스트
+
+function solution(N, stages) {
+    var answer = [];
+    let stayPlayer = [];
+    let human = stages.length;
+    for(let i=0;i<=N;i++){
+        stayPlayer[i] = 0;
+    }
+    stages.forEach(ele => {
+        if(ele<=N) {
+            stayPlayer[ele]++;
+        }
+    });
+    stayPlayer.shift();
+    let index =1;
+    stayPlayer.forEach(ele => {
+        if(ele!=0) {
+            answer.push([index++, ele / human]);
+            human -= ele;
+        }
+        else if(ele==0){
+            answer.push([index++,0]);
+        }
+    });
+    answer.sort((a,b)=>
+    {
+        if(b[1]==a[1]){
+            return a[0]-b[0];
+        }
+        else {
+            return b[1]-a[1];
+        }
+    });
+    let res=[];
+    answer.forEach(ele=> {
+        res.push(ele[0]);
+    });
+    return res;
+}
---
+++ programmers/LEVEL1/예산.js
@@ -0,0 +1,13 @@
+﻿function solution(d, budget) {
+    var answer = 0;
+    let sum=0;
+    d.sort((a,b) => a-b);
+    for(let i of d){
+        if(i+sum <=budget){
+            sum+=i;
+            answer++;
+        }
+        else break;
+    }
+    return answer;
+}
