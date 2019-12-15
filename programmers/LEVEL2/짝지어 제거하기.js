9246997e5d6d7dfec4cf4a5ec47cd3452f95864f
dobidugi <dobidugi@gmail.com>
2019-12-16 02:43:00 +0900

    작지어 제거하기

---
+++ programmers/LEVEL2/짝지어 제거하기.js
@@ -0,0 +1,15 @@
+﻿function solution(s)
+{
+    if(s.length%2!=0) return 0;
+    let stack = [];
+    for(let i=0;i<s.length;i++) {
+        if (stack.length != 0 && stack[stack.length - 1] == s[i])
+            stack.pop();
+        else
+            stack.push(s[i]);
+        
+    }
+    if (stack.length == 0)
+            return 1;
+    else return 0;
+}
